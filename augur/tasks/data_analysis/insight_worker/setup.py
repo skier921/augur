ef8fbe224c17eb59dc2e3c001135f552c5563be9
Sean P. Goggins <s@goggins.com>
2023-09-05 09:26:04 -0500

    OSX Scipy version issue with Python3.11

--- augur/tasks/data_analysis/insight_worker/setup.py
+++ augur/tasks/data_analysis/insight_worker/setup.py
@@ -29,7 +29,7 @@ setup(
         'requests==2.28.0',
         'psycopg2-binary==2.9.3',
         'click==8.0.3',
-        'scipy>=1.7.3',
+        'scipy>=1.8.1',#1.7.3',
         'sklearn==0.0',
         'numpy==1.22.0', 
     ],
