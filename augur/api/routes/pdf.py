8120f0e947d66d55a7930bc558c7595898f2349c
Sean P. Goggins <s@goggins.com>
2023-09-27 17:02:54 -0500

    fixing imports

--- augur/api/routes/pdf.py
+++ augur/api/routes/pdf.py
@@ -2,38 +2,22 @@
 """
 Creates routes for user functionality
 """
+import base64
+from augur.api.metrics.repo_meta import license_files
+from augur.api.metrics.insight import top_insights
+from augur.api.routes import AUGUR_API_VERSION
+from ..server import app, route_transform
+import sqlalchemy as s
+import pandas as pd
+from augur.api.util import metric_metadata
+import os
+import requests
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
-import logging
-import requests
-import os
-import base64
-import time
-import secrets
-import pandas as pd
 from flask import request, Response, jsonify, session, render_template
-from flask_login import login_user, logout_user, current_user, login_required
-from werkzeug.security import check_password_hash
-from sqlalchemy.sql import text
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.exc import NoResultFound
-from augur.application.db.session import DatabaseSession
-from augur.tasks.github.util.github_task_session import GithubTaskSession
-from augur.util.repo_load_controller import RepoLoadController
-from augur.api.util import api_key_required, ssl_required
-
-from augur.application.db.models import User, UserRepo, UserGroup, UserSessionToken, ClientApplication, RefreshToken
-from augur.application.config import get_development_flag
-from augur.tasks.init.redis_connection import redis_connection as redis
-from ..server import app, engine
-
-logger = logging.getLogger(__name__)
-development = get_development_flag()
-Session = sessionmaker(bind=engine)
-
+from augur.api.metrics.repo_meta import license_files
+from augur.api.metrics.insight import top_insights
 from augur.api.routes import AUGUR_API_VERSION
-
-AUGUR_API_VERSION = 'api/unstable'
 
 @app.route('/{}//generate_pdf/<name>'.format(AUGUR_API_VERSION), methods=["GET"])
     def generate_pdf(name):
