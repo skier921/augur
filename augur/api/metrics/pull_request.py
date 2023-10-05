0c70d7a92e363c4f691c1c96bc297d35be7d4487
Sean P. Goggins <s@goggins.com>
2023-10-05 12:31:50 -0500

    pr api patch draft

--- augur/api/metrics/pull_request.py
+++ augur/api/metrics/pull_request.py
@@ -11,6 +11,80 @@ from augur.api.util import register_metric
 from ..server import engine
 
 @register_metric()
+def pull_requests_first_time_opened(repo_group_id, repo_id=None, period='day', begin_date=None, end_date=None):
+    """
+    Returns a timeseries of the count of persons opening an issue for the first time.
+
+    :param repo_id: The repository's id
+    :param repo_group_id: The repository's group id
+    :param period: To set the periodicity to 'day', 'week', 'month' or 'year', defaults to 'day'
+    :param begin_date: Specifies the begin date, defaults to '1970-1-1 00:00:00'
+    :param end_date: Specifies the end date, defaults to datetime.now()
+    :return: DataFrame of persons/period
+    ?begin_date=2023-01-01&end_date=2023-06-01-01 for URL parameters
+    """
+
+    if not begin_date:
+        begin_date = '1970-1-1 00:00:01'
+    if not end_date:
+        end_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    if repo_id:
+        prNewContributor = s.sql.text("""
+                SELECT
+                    date_trunc(:period, new_date::DATE) as pr_date, --day, week, month, year 
+                    COUNT(*),
+                    repo_name
+                FROM (
+                    SELECT
+                            pr_augur_contributor_id,
+                            MIN(pull_requests.pr_created_at) AS new_date,
+                            repo_name
+                    FROM
+                            pull_requests JOIN repo ON pull_requests.repo_id = repo.repo_id  
+                    WHERE
+                            pull_requests.repo_id = :repo_id
+                            AND pull_requests.pr_created_at BETWEEN :begin_date AND :end_date
+                    GROUP BY pr_augur_contributor_id, repo_name
+                ) as abc
+                GROUP BY pr_date, repo_name
+                ORDER BY pr_date
+            ) as abc
+            GROUP BY issue_date, repo_name
+            ORDER BY issue_date
+        """)
+        results = pd.read_sql(prNewContributor, engine, params={'repo_id': repo_id, 'period': period,
+                                                                    'begin_date': begin_date, 'end_date': end_date})
+    else:
+        issueNewContributor = s.sql.text("""
+            SELECT
+                repo.repo_id,
+                repo_name,
+                date_trunc(:period, new_date::DATE) as issue_date,
+                COUNT(gh_user_id)
+            FROM (
+                SELECT
+                    repo_id,
+                    gh_user_id,
+                    MIN(created_at) AS new_date
+                FROM
+                    issues
+                WHERE
+                    issues.pull_request IS NULL 
+                    AND repo_id in (SELECT repo_id FROM repo WHERE repo_group_id=:repo_group_id)
+                    AND created_at BETWEEN :begin_date AND :end_date
+                GROUP BY gh_user_id, repo_id
+            ) as abc, repo
+            WHERE repo.repo_id= abc.repo_id
+            GROUP BY repo.repo_id, issue_date
+            ORDER BY issue_date
+        """)
+        results = pd.read_sql(issueNewContributor, engine,
+                            params={'repo_group_id': repo_group_id, 'period': period,
+                                    'begin_date': begin_date, 'end_date': end_date})
+    return results
+
+@register_metric()
 def pull_requests_merge_contributor_new(repo_group_id, repo_id=None, period='day', begin_date=None, end_date=None):
     """
     Returns a timeseries of the count of persons contributing with an accepted commit for the first time.
